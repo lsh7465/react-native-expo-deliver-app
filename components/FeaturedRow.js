9512afa3616d0d3abd5e2f13ac8c06bd8e9b54f2
lsh7465 <lsh746527@gmail.com>
2024-03-12 17:31:16 +0900

    FeaturedRow 생성

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import { Text } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getFeaturedRestaurantById } from "../api";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCard from "./RestaurantCard";
 
-const FeaturedRow = () => {
+const FeaturedRow = ({ id, title, description }) => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    getFeaturedRestaurantById(id)
+      .then((data) => {
+        setRestaurants(data?.restaurants);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
   return (
-    <Text>FeaturedRow</Text>
-  )
-}
+    <View>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB" />
+      </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        {restaurants?.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant._id}
+            id={restaurant._id}
+            imgUrl={restaurant.image}
+            title={restaurant.name}
+            rating={restaurant.rating}
+            genre={restaurant.type?.name}
+            address={restaurant.address}
+            shortDescription={restaurant.short_description}
+            dishes={restaurant.dishes}
+            long={restaurant.long}
+            lat={restaurant.lat}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
-export default FeaturedRow
+export default FeaturedRow;
+
+const styles = StyleSheet.create({});
