64dbaa2269a1f68ebca686a8e1297a6899990615
lsh7465 <lsh746527@gmail.com>
2024-03-12 17:31:56 +0900

    RestaurantCard 생성

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,10 +1,62 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-const RestaurantCard = () => {
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import { StarIcon } from "react-native-heroicons/outline";
+import { MapPinIcon } from "react-native-heroicons/solid";
+const RestaurantCard = ({
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  shortDescription,
+  dishes,
+  long,
+  lat,
+}) => {
+  const navigation = useNavigation();
   return (
-    <Text>RestaurantCard</Text>
-  )
-}
+    <TouchableOpacity
+      className="mr-3 shadow-black bg-white rounded-lg"
+      onPress={() =>
+        navigation.navigate("Restaurant", {
+          id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          shortDescription,
+          dishes,
+          long,
+          lat,
+        })
+      }
+    >
+      <Image
+        source={{ url: urlFor(imgUrl).url() }}
+        className="w-64 rounded-t-lg h-36"
+      />
+      <View className="px-3 pb-4">
+        <Text className="pt-2 text-lg font-bold">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-xs text-green-500">{rating}</Text>
+            {genre}
+          </Text>
+        </View>
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon color="gray" opacity={0.2} size={22} />
+          <Text className="text-xs text-gray-500">Nearby {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default RestaurantCard
+export default RestaurantCard;
+
+const styles = StyleSheet.create({});
